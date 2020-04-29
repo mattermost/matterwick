19aa491417cca8722bb84f86c10750fe094ed0f6
Carlos Panato <ctadeu@gmail.com>
2020-04-29 09:51:33 +0200

    set new context when waiting for TE

--- server/spinwick.go
+++ server/spinwick.go
@@ -110,7 +110,9 @@ func (s *Server) createSpinWick(pr *model.PullRequest, size string, withLicense 
 			s.sendGitHubComment(pr.RepoOwner, pr.RepoName, pr.Number, "Enterprise Edition Image not available in the 30 minutes timeframe, checking the Team Edition Image and if available will use that.")
 			//fallback to TE
 			image := "mattermost/mattermost-team-edition"
-			prNew, errImage = s.Builds.waitForImage(ctxEnterprise, s, reg, pr, image)
+			ctxTeam, cancelTeam := context.WithTimeout(context.Background(), 30*time.Minute)
+			defer cancelTeam()
+			prNew, errImage = s.Builds.waitForImage(ctxTeam, s, reg, pr, image)
 			if errImage != nil {
 				mlog.Warn("Did not find TE image", mlog.Int("pr", pr.Number), mlog.String("repo_owner", pr.RepoOwner), mlog.String("repo_name", pr.RepoName), mlog.String("sha", pr.Sha))
 				return request.WithError(errors.Wrap(errDocker, "unable to get docker registry client")).ShouldReportError()
